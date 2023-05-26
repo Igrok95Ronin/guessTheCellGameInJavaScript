a55b8fa184b37dda7e8cb4388c52d096904f453f
xl92@DESKTOP-F8KSG17 <xl92@mail.ru>
2023-05-26 11:37:08 +0300

    update_1.1

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -23,7 +23,6 @@ let counter : number = 1;
                 td.textContent = `${counter}`
                 counter++
 
-
                 compareValues(td, resultRandom)
             }
         }
@@ -32,67 +31,64 @@ let counter : number = 1;
 
 
 /* Получение случайного целого числа между min и max: */
-function getRandomInt(min : number, max : number) : number[] {
-    let arrNumbers : number[] = []
-    while (arrNumbers.length < 10) {
-    let randomNum : number = Math.floor(Math.random() * (max - min + 1)) + min;
-            if (!arrNumbers.includes(randomNum)) {
-                arrNumbers.push(randomNum)
+    function getRandomInt(min : number, max : number) : number[] {
+        let arrNumbers : number[] = []
+        while (arrNumbers.length < 10) {
+        let randomNum : number = Math.floor(Math.random() * (max - min + 1)) + min;
+                if (!arrNumbers.includes(randomNum)) {
+                    arrNumbers.push(randomNum)
+            }
         }
+        return arrNumbers
     }
-    return arrNumbers
-}
 
-let count : number = 10,
-    time : number = 60;
-    times.textContent = `${time}`
+    let count : number = 10,
+        time : number = 60;
+        times.textContent = `${time}`
 
 //Если вышло время вы проиграли
-function timeIsOver(time : number,times: HTMLElement) {
-    let id = setInterval(() => {
-        time--
-        if(time < 0) {
-            clearInterval(id)
-            alert("Вы не успели")
-            location.reload()
-        }
-        times.textContent = `${time}`
-    }, 1000)
-
-}
-
-//Игра начинается при клике на любое место
-window.addEventListener('click', () => {
-    timeIsOver(time, times)
-})
-
-//Если нашли все ячейки Выгрыш
-counts.textContent = `${count}`
-function quantity() {
-    count--
-    counts.textContent = `${count}`
-    console.log(count);
-    if(count == 0) {
-        setTimeout(()=>{
-            alert("Выгрыш! Игра окончена")
-            location.reload();
+    function timeIsOver(time : number,times: HTMLElement) {
+        let id = setInterval(() => {
+            time--
+            if(time < 0) {
+                clearInterval(id)
+                alert("Вы не успели")
+                location.reload()
+            }
+            times.textContent = `${time}`
         }, 1000)
     }
-}
+
+//Игра начинается при клике на любое место
+    window.addEventListener('click', () => {
+        timeIsOver(time, times)
+    })
+
+//Если нашли все ячейки Выгрыш
+    counts.textContent = `${count}`
+    function quantity() {
+        count--
+        counts.textContent = `${count}`
+        console.log(count);
+        if(count == 0) {
+            setTimeout(()=>{
+                alert("Выгрыш! Игра окончена")
+                location.reload();
+            }, 1000)
+        }
+    }
 
 // Дбавляет обработчик события клика к элементу ячейки таблиц
-function compareValues(td : HTMLTableCellElement, resultRandom : number[]) {
-    td.addEventListener('click', function() {
-        let thisTdValue : number = +this.textContent
-        resultRandom.forEach(elem => {
-            if (thisTdValue === elem) {
-                td.classList.add('active')
-                quantity()
-            }
+    function compareValues(td : HTMLTableCellElement, resultRandom : number[]) {
+        td.addEventListener('click', function() {
+            let thisTdValue : number = +this.textContent
+            resultRandom.forEach(elem => {
+                if (thisTdValue === elem) {
+                    td.classList.add('active')
+                    quantity()
+                }
+            })
         })
-    })
-}
-
-
+    }
 
 })
